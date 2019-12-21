8e039ac4309d17c6d0fe58d095e71ca062f8498f
khavernathy <khaverim7@gmail.com>
2019-12-21 14:08:23 -0500

    rename output file in listen.py to currtime

--- listen.py
+++ listen.py
@@ -8,13 +8,13 @@ import datetime
 
 serial_port = '/dev/ttyACM1' #'COM7' # /dev/cu.usbmodem1411'  #'/dev/cu.usbmodem1421'; #/dev/ttyACM0';
 baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
-write_to_file_path = "output.txt";
+write_to_file_path = "output_" + str(datetime.datetime.now() ) + ".txt";
 
 output_file = open(write_to_file_path, "w+", buffering=1);
 ser = Serial(serial_port, baud_rate)
 while True:
     line = ser.readline();
     line = line.decode("utf-8") #ser.readline returns a binary, convert to string
-    print(line);
     now = datetime.datetime.now()
+    print(str(now) + "    " + line);
     output_file.write( line );   #str(now)+line);
