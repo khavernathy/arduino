20f6ac4e599bf4b9de807e690e1e32c5c858b8fa
khavernathy <khaverim7@gmail.com>
2020-01-08 02:09:34 -0500

    add unixtime to est convert writer

--- translate.py
+++ translate.py
@@ -8,7 +8,7 @@ f2 = open("DATA/T3.conv", "w")
 
 for i in np.arange( 0, len(x), 1):
     s = x[i].split()
-    f2.write( str( convert.convert( s[0] ) ) + " " + str( s[1] ) + "\n" ) 
+    f2.write( str( convert.convert( s[0] ) ) + ", " + str( s[1] ) + "\n" ) 
 
 f2.close()
 f.close()
