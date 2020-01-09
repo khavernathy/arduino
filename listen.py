0a521eddcc8194226b4e6fadb57d523c0de7cdbe
khavernathy <khaverim7@gmail.com>
2020-01-09 01:31:16 -0500

    make filename unix time listen.py

--- listen.py
+++ listen.py
@@ -5,10 +5,13 @@
 ## pip3 install pyserial
 from serial import Serial
 import datetime
+import time
 
 serial_port = '/dev/ttyACM1' #'COM7' # /dev/cu.usbmodem1411'  #'/dev/cu.usbmodem1421'; #/dev/ttyACM0';
 baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
-write_to_file_path = "output_" + str(datetime.datetime.now() ) + ".txt";
+#write_to_file_path = "output_" + str(datetime.datetime.now() ) + ".txt";
+write_to_file_path = "output_" + str( int(time.time()) ) + ".txt";
+
 
 output_file = open(write_to_file_path, "w+", buffering=1);
 ser = Serial(serial_port, baud_rate)
