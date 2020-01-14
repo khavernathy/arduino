78137b32fc892849fb76684662c79d4aa2a980c6
Douglas M Franz <dfranz@mail.usf.edu>
2020-01-14 18:02:23 -0500

    update grpahing

--- DATA/makegraphs.sh
+++ DATA/makegraphs.sh
@@ -6,12 +6,39 @@
 #xmgrace -hdevice PNG -hardcopy -printfile RH.png    RH_*.dat 
 
 while [ 1 ]; do
-export DISPLAY=:0
-python="/usr/bin/python3.6"
-$python graph.py T3.png     T3_*.dat
-$python graph.py LIGHT.png  LIGHT_*.dat
-$python graph.py RH.png     RH_*.dat
+	export DISPLAY=:0
+	python="/usr/bin/python3.6"
+	$python timegraph.py T3.png     T3_*.dat
+	$python timegraph.py LIGHT.png  LIGHT_*.dat
+	$python timegraph.py RH.png     RH_*.dat
 
-bash coorelations.sh   # will make 3 more for coorelations of Temp,Humid,Light
-sleep 60;
+
+	[ -f t ] && rm t;
+	touch t;
+	for x in ./T3_*; do
+		cat "$x" | awk {'print $2'} >> t
+	done
+	[ -f l ] && rm l;
+	touch l;
+	for x in ./LIGHT_*; do
+		cat "$x" | awk {'print $2'} >> l
+	done
+	[ -f h ] && rm h;
+	touch h;
+	for x in ./RH_*; do
+		cat "$x" | awk {'print $2'} >> h
+	done
+
+	paste t l > tl;
+	paste t h > th;
+	paste l h > lh;
+	rm l t h;
+	python="/usr/bin/python3.6"
+	$python coorelations.py TL.png tl
+	$python coorelations.py TH.png th
+	$python coorelations.py LH.png lh
+
+	rm tl th lh;
+
+	sleep 60;
 done
