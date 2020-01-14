8e71e5754ad3252b7ed9e998d96a1fc333f08358
khavernathy <khaverim7@gmail.com>
2020-01-14 12:39:39 -0500

    send aruino data to website

--- grab_data.sh
+++ grab_data.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-
 while [ 1 ]; do
 
-    scp sci:~/arduino/output_202* .
-    
-    cat output_202* | awk {'print $1, $2'} > DATA/T1.dat
-    cat output_202* | awk {'print $1, $3'} > DATA/RH.dat
-    cat output_202* | awk {'print $1, $4'} > DATA/LIGHT.dat
-    cat output_202* | awk {'print $1, $5'} > DATA/T2.dat
-    cat output_202* | awk {'print $1, $6'} > DATA/T3.dat
+    scp sci:~/arduino/output_* .
+
+    c=0
+    for x in ./output_*; do
+        cat "$x" | awk {'print $1, $2'} > DATA/T1_$c.dat
+        cat "$x" | awk {'print $1, $3'} > DATA/RH_$c.dat
+        cat "$x" | awk {'print $1, $4'} > DATA/LIGHT_$c.dat
+        cat "$x" | awk {'print $1, $5'} > DATA/T2_$c.dat
+        cat "$x" | awk {'print $1, $6'} > DATA/T3_$c.dat
+        let c=$c+1
+    done
 
     # to convert timestamp -> UTC -> EST
     #python -c "stmp=1577035548; import datetime;  print( datetime.datetime.utcfromtimestamp( stmp + 3600 ).strftime('%d %b %Y %H:%M:%S') );"
+
+    scp DATA/*dat web:/var/www/html/arduino/DATA
 
     sleep 60;
 done
